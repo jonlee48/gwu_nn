57bb3868430f4125a18dcd749a31e4f417c61110
jonlee48 <jonathan.lee48@gmail.com>
2021-12-06 19:47:10 -0500

    additions to readme + convolutional layer

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ The George Washington University. It is a basic implementation that provides a n
 functions, and loss functions for students to explore how networks work at a low level. 
 
 # Convolutional Neural Network
-This branch providees CNN support to the GWU NN library, implemented by Jonathan Lee.
+This branch adds CNN functionality to the GWU NN library for the final course project. Features implemented in 
+this branch by Jonathan Lee. 
+
+This branch will include code for inputting the image, implementing convolutional layers, pooling layers, and a flattening layer. The new layers will have to support forward and backward propagation as implemented in the GWU NN. I plan on training a network on the MNIST handwritten digits dataset and comparing the model accuracy against some existing models.
+
+## Resources
+### Articles:
+-	Building a CNN using Numpy from scratch (includes python code) https://towardsdatascience.com/building-convolutional-neural-network-using-numpy-from-scratch-b30aac50e50a 
+-	cnn-numpy (includes python code) https://github.com/neuron-whisperer/cnn-numpy 
+### Textbooks:
+- Deep Learning from Scratch with Python from First Principles by Seth Weidman, Chapter 5 CNNs
+  - Explains the math of forward and backward propagation as well as some python code
+- Python Machine Learning by Sebastian Reschka, Chapter 15 CNNs
+  - Has some good visuals on how CNNs work
--- gwu_nn/layers.py
+++ gwu_nn/layers.py
@@ -109,3 +109,64 @@ class Dense(Layer):
         if self.add_bias:
             self.bias -= learning_rate * output_error
         return input_error
+
+class Convolutional(Layer):
+
+    def __init__(self, output_size, add_bias=False, activation=None, input_size=None):
+        super().__init__(activation)
+        self.type = None
+        self.name = "Convolutional"
+        self.input_size = input_size
+        self.output_size = output_size
+        self.add_bias = add_bias
+
+
+    def init_weights(self, input_size):
+        """Initialize the weights for the layer based on input and output size
+
+        Args:
+            input_size (np.array): dimensions for the input array
+        """
+        if self.input_size is None:
+            self.input_size = input_size
+
+        self.weights = np.random.randn(input_size, self.output_size) / np.sqrt(input_size + self.output_size)
+        if self.add_bias:
+            self.bias = np.random.randn(1, self.output_size) / np.sqrt(input_size + self.output_size)
+
+
+    @apply_activation_forward
+    def forward_propagation(self, input):
+        """Applies the forward propagation for a densely connected layer. This will compute the dot product between the
+        input value (calculated during forward propagation) and the layer's weight tensor.
+
+        Args:
+            input (np.array): Input tensor calculated during forward propagation up to this layer.
+
+        Returns:
+            np.array(float): The dot product of the input and the layer's weight tensor."""
+        self.input = input
+        output = np.dot(input, self.weights)
+        if self.add_bias:
+            return output + self.bias
+        else:
+            return output
+
+    @apply_activation_backward
+    def backward_propagation(self, output_error, learning_rate):
+        """Applies the backward propagation for a densely connected layer. This will calculate the output error
+         (dot product of the output_error and the layer's weights) and will calculate the update gradient for the
+         weights (dot product of the layer's input values and the output_error).
+
+        Args:
+            output_error (np.array): The gradient of the error up to this point in the network.
+
+        Returns:
+            np.array(float): The gradient of the error up to and including this layer."""
+        input_error = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+
+        self.weights -= learning_rate * weights_error
+        if self.add_bias:
+            self.bias -= learning_rate * output_error
+        return input_error
